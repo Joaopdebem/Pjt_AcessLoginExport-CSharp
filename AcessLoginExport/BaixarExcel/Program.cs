ed83f1c6e845b345893481f3b4d71212cec43d46
João Pedro de Bem <jpdebem@gmail.com>
2023-09-22 09:40:22 -0300

    Ajuste de arquivos

--- AcessLoginExport/BaixarExcel/Program.cs
+++ AcessLoginExport/BaixarExcel/Program.cs
@@ -19,10 +19,10 @@ class Program
 
         using (var driver = new ChromeDriver(chromeDriverPath, chromeOptions))
         {
-            driver.Navigate().GoToUrl("http://34.235.5.35:8080/los-dashboard");
+            driver.Navigate().GoToUrl("http://34.235.5.35:8080/los-dashboard"); // url que deseja
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             Thread.Sleep(2000);
-            
+
             var timeout = TimeSpan.FromSeconds(10);
             var pollInterval = TimeSpan.FromMilliseconds(500);
             var startTime = DateTime.Now;
@@ -31,7 +31,7 @@ class Program
 
             while (DateTime.Now - startTime < timeout)
             {
-                try
+                try // tenta preencher o campo login
                 {
                     element1 = driver.FindElement(By.CssSelector("#login\\:name"));
                     if (element1.Displayed)
@@ -39,8 +39,8 @@ class Program
                         break;
                     }
                 }
-                catch (NoSuchElementException){}
-                catch (StaleElementReferenceException){}
+                catch (NoSuchElementException) { }
+                catch (StaleElementReferenceException) { }
 
                 Thread.Sleep(pollInterval);
             }
@@ -48,7 +48,7 @@ class Program
             if (element1 != null)
             {
                 element1.Click();
-                element1.SendKeys("wagner.reis");
+                element1.SendKeys("teste");
             }
 
             startTime = DateTime.Now;
@@ -56,7 +56,7 @@ class Program
 
             while (DateTime.Now - startTime < timeout)
             {
-                try
+                try // tenta preencher o campo senha
                 {
                     element2 = driver.FindElement(By.CssSelector("#login\\:password"));
                     if (element2.Displayed)
@@ -73,24 +73,24 @@ class Program
             if (element2 != null)
             {
                 element2.Click();
-                element2.SendKeys("W4gn3r#08");
+                element2.SendKeys("123");
                 element2.SendKeys(Keys.Enter);
             }
             Thread.Sleep(3000);
 
-            driver.Navigate().GoToUrl("http://34.235.5.35:8080/los-dashboard/products/expirationDate.xhtml");
-            wait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete"));
+            driver.Navigate().GoToUrl("http://34.235.5.35:8080/los-dashboard/products/expirationDate.xhtml"); // navega para outra parte do site
+            wait.Until(d => ((IJavaScriptExecutor)d).ExecuteScript("return document.readyState").Equals("complete")); // Aguarda carregar aquela p[agina do site
             Thread.Sleep(3000);
-            
+
             ((IJavaScriptExecutor)driver).ExecuteScript("console.clear();");
 
-            string javascriptCommand = "mojarra.jsfcljs(document.getElementById('expirationForm'), {'expirationForm:expirationDataTableId:j_idt404':'expirationForm:expirationDataTableId:j_idt404'}, '');";
+            string javascriptCommand = "mojarra.jsfcljs(document.getElementById('expirationForm'), {'expirationForm:expirationDataTableId:j_idt404':'expirationForm:expirationDataTableId:j_idt404'}, '');"; // executa o classe que exporta o excel que preciso
 
             ((IJavaScriptExecutor)driver).ExecuteScript(javascriptCommand);
-            Thread.Sleep(5000);        
+            Thread.Sleep(5000);
 
-            string sourcePath = @"C:\Users\jpdeb\Downloads\";
-            string targetPath = @"C:\Users\jpdeb\OneDrive\Área de Trabalho\Aviso Encapsulados";
+            string sourcePath = @"C:\"; // local que e baixado o arquivo
+            string targetPath = @"C:\"; // local para onde quero que mova
 
             DirectoryInfo dir = new DirectoryInfo(sourcePath);
 
@@ -108,6 +108,6 @@ class Program
                 File.Move(file.FullName, destFile);
             }
 
-        }    
+        }
     }
 }
